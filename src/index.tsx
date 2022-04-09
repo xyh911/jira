fed9f9dc8aeb504c07d6ebf0b9a775f5f0c1f70d
xyh911 <1259103667@qq.com>
2022-04-09 15:35:22 +0800

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <div>12</div>
   </React.StrictMode>,
   document.getElementById("root")
 );
