b971786abf118c211cea0f42ebf317b19222b1bd
xyh911 <1259103667@qq.com>
2022-04-19 19:44:26 +0800

    chore:用useCallback解决无限渲染问题

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,6 +1,7 @@
 import qs from "qs";
 import * as auth from "auth-provider";
 import { useAuth } from "context/auth-context";
+import { useCallback } from "react";
 interface Config extends RequestInit {
   token?: string;
   data?: object;
@@ -42,6 +43,9 @@ export const http = async (
 
 export const useHttp = () => {
   const { user } = useAuth();
-  return (...[endpoint, config]: Parameters<typeof http>) =>
-    http(endpoint, { ...config, token: user?.token });
+  return useCallback(
+    (...[endpoint, config]: Parameters<typeof http>) =>
+      http(endpoint, { ...config, token: user?.token }),
+    [user?.token]
+  );
 };
